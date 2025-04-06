2908073684102415d48c84e426cd3a6505057522
Mahin556 <mahinraza556@gmail.com>
2025-04-06 14:47:31 +0530

    Initial commit

---
+++ webmail/class/mime/SMimeMessage.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * SMimeMessage.class.php
+ *
+ * This file contains class needed to handle SMIME mime messages.
+ *
+ * @link http://www.ietf.org/html.charters/smime-charter.html
+ * @copyright 2003-2025 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: SMimeMessage.class.php 15030 2025-01-02 02:06:04Z pdontthink $
+ * @package squirrelmail
+ * @subpackage mime
+ * @since 1.3.2
+ */
+
+/**
+ * Unimplemented class.
+ * @package squirrelmail
+ * @subpackage mime
+ * @todo implement smime parsing
+ */
+class SMimeMessage {
+
+}
+
---
+++ webmail/doc/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * index.php
+ *
+ * Redirects to the index.html file.
+ *
+ * @copyright 1999-2025 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: index.php 15030 2025-01-02 02:06:04Z pdontthink $
+ * @package squirrelmail
+ */
+
+header('Location: index.html');
+
